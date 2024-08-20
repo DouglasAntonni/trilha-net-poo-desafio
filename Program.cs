fd247723d2eadd10d00eb8b351ce9ed93aa0ce26
Douglas Antonni Alves De Lima <127199046+DouglasAntonni@users.noreply.github.com>
2024-08-20 16:36:42 -0300

    Desafio de projeto finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+       
+        Smartphone iphone = new Iphone("123456789", "iPhone 14", "26765355626652", 128);
+        Smartphone nokia = new Nokia("987654321", "Nokia Lumia", "72986523416", 64);
+
+        
+        Console.WriteLine("Testando iPhone:");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\nTestando Nokia:");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Telegram");
+
+       
+
